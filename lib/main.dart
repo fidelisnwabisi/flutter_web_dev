29d50d33f0f4178f8f0c831d7103648987421b26
fidelis <fidelisnwabisi@gmail.com>
2023-07-15 06:19:48 +0100

    added a responsive menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'my_custom_appbar.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(home: MyApp()), // use MaterialApp
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,87 +18,35 @@ class MyApp extends StatelessWidget {
       backgroundColor: Colors.pink.shade50,
       body: ListView(
         children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
-            height: 80,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.circular(40)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "My Design Buddy",
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      fontWeight: FontWeight.bold, color: Colors.black),
-                ),
-                Row(
-                  children: [
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Pricing",
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
+          MediaQuery.of(context).size.width > 720
+              ? MyCustomAppBar()
+              : Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 60),
+                  height: 80,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
                       ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Work",
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
+                      borderRadius: BorderRadius.circular(40)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "My Design Buddy",
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            fontWeight: FontWeight.bold, color: Colors.black),
                       ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "About Me",
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "FAQ",
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    ElevatedButton(
-                        style: ButtonStyle(
-                          overlayColor: MaterialStateProperty.all(
-                              Colors.black.withOpacity(0.8)),
-                          backgroundColor: MaterialStateProperty.all(
-                              Colors.black.withOpacity(0.7)),
-                          padding: MaterialStateProperty.all(
-                              EdgeInsets.symmetric(
-                                  horizontal: 40, vertical: 20)),
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20)),
-                          ),
-                        ),
+                      IconButton(
                         onPressed: () {},
-                        child: Text(
-                          "LogIn",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ))
-                  ],
-                )
-              ],
-            ),
-          )
+                        icon: Icon(Icons.menu),
+                      )
+                    ],
+                  ),
+                ),
         ],
       ),
     );
